2230ca2032fd28d738ad58d0587ab43d189857d9
leonardoalves <leonardoalvesalves09@gmail.com>
2018-10-26 21:26:31 -0200

    criei um teste no java, deu erro, mas a gente vai arrumar,pas

---
+++ habbibs/src/br/com/habbibs/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.habbibs;
+public class Main{
+    public static void main(string[] args) {
+        System.out.println("Sistema criado com sucesso");
+    }
+            
+            
+}
+/**
+ *
+ * @author LAB_ETECIA
+ */
+public class Main {
+    
+}
